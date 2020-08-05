5c24944338f0d1a297c8a0b55ee73243e9e75adb
devppratik <pratikpanda24@gmail.com>
2020-08-05 14:42:20 +0530

    Portfolio v1.1 Fixed Bugs

--- portfolio/scripts/index.js
+++ portfolio/scripts/index.js
@@ -8,19 +8,19 @@ ham.addEventListener('click', (e) => {
 if (window.location.pathname === "/" || window.location.pathname === "/index.html") {
     const btn = document.querySelector('.button.page-link');
     btn.addEventListener('click', () => {
-        window.open("/projects.html", name = "_self");
+        window.open("/portfolio/projects.html", name = "_self");
     });
 }
 let curr = window.location.pathname;
 if (curr === "/" || curr === "/index.html") {
     home.classList.add("active")
 }
-else if (curr === "/skills.html") {
+else if (curr === "/portfolio/skills.html") {
     home.nextElementSibling.classList.add("active");
 }
-else if (curr === "/projects.html") {
+else if (curr === "/portfolio/projects.html") {
     home.nextElementSibling.nextElementSibling.classList.add("active");
 }
-else if (curr === "/contact.html") {
+else if (curr === "/portfolio/contact.html") {
     home.nextElementSibling.nextElementSibling.nextElementSibling.classList.add("active");
 }

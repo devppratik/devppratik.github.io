6b115e57467454b118ddfa4485b0ccf968fdcf42
devppratik <pratikpanda24@gmail.com>
2020-08-06 14:19:32 +0530

    Portfolio v1.8 Finalized

--- portfolio/scripts/index.js
+++ portfolio/scripts/index.js
@@ -1,24 +1,34 @@
 const ham = document.querySelector('#mobile-link');
 const navbar = document.querySelector('.nav');
 const logo = document.querySelector('.logo-main');
-
-window.addEventListener('load',function(){
-    document.querySelector('body').classList.add("loaded")
-  });
 const home = document.querySelector('.home-link');
+let curr = window.location.pathname;
 
+// Pre-Loader Script
+window.addEventListener('load', function () {
+    document.querySelector('body').classList.add("loaded")
+});
+
+// Mobile Nav Bar Script
 ham.addEventListener('click', (e) => {
     e.preventDefault();
     navbar.classList.toggle('show');
 });
 
+//Home Page Button Click Event
 if (window.location.pathname === "/" || window.location.pathname === "/index.html") {
     const btn = document.querySelector('.button.page-link');
     btn.addEventListener('click', () => {
         window.open("/portfolio/projects.html", name = "_self");
     });
 }
-let curr = window.location.pathname;
+
+//Logo Click Event
+logo.addEventListener('click', () => {
+    window.open("/index.html", name = "_self");
+})
+
+// Adding Active Class to Current Page
 if (curr === "/" || curr === "/index.html") {
     home.classList.add("active")
 }
@@ -32,6 +42,37 @@ else if (curr === "/portfolio/contact.html") {
     home.nextElementSibling.nextElementSibling.nextElementSibling.classList.add("active");
 }
 
-logo.addEventListener('click' , ()=>{
-    window.open("/index.html", name = "_self");
-})
+// Contact Form Event
+if (curr === "/portfolio/contact.html") {
+    const form = document.querySelector('.contact-form');
+    const popup = document.querySelector('.popup-wrapper');
+    const close = document.querySelector('.popup-close')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const now = new Date();
+        const details = {
+            name: form.name.value,
+            email: form.email.value,
+            subject: form.subject.value,
+            message: form.message.value,
+            date: firebase.firestore.Timestamp.fromDate(now)
+        }
+        db.collection('orders').add(details).then(() => {
+            // window.alert("Message added")
+            //Show Popup
+            popup.style.display = 'block';
+            close.addEventListener('click', () => {
+                popup.style.display = 'none';
+            })
+            popup.addEventListener('click', () => {
+                popup.style.display = 'none';
+            })
+            setTimeout(() => {
+                popup.style.display = 'none';
+            }, 5000)
+            form.reset();
+        }).catch(err => {
+            console.log(err)
+        })
+    })
+}
